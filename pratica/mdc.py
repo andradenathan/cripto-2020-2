51130ac80c9e574e0197fa4095ccf685fc79274e
Nathan Andrade <42661561+andradenathan@users.noreply.github.com>
2021-05-03 14:05:26 -0300

    + Desencripta mensagem na Cifra de César e TFA

--- pratica/mdc.py
+++ pratica/mdc.py
@@ -9,10 +9,11 @@ def mdc_ingenuo(a, b):
 
 
 def algoritmo_euclidiano(a, b):
+    print(a, b)
     if b == 0:
         return a
 
     else:
         return algoritmo_euclidiano(b, a % b)
 
-print(algoritmo_euclidiano(27282828221113327892728282822111332789272828282211133278927282828221113327891133, 3223427282828221113328889978927282828221113327892728282822111332789644136612423976))
+print(algoritmo_euclidiano(2244, 7980))
---
+++ pratica/tfa.py
@@ -0,0 +1,20 @@
+# Teorema Fundamental da Aritmética
+
+def menor_fator(n):
+    for divisor in range(2, n+1):
+        if divisor ** 2 > n:
+            return n
+
+        if n % divisor == 0:
+            return divisor
+
+def fatoracao_em_primos(n):
+    fatorar = n
+    lista = []
+    while fatorar > 2:
+        p = menor_fator(n)
+        fatorar //= p # divisão inteira
+        lista.append(p)
+    return lista
+
+print(fatoracao_em_primos(11*131))
