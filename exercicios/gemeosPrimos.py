311e81b429f037e784569afb6b582146d2802d02
Nathan Andrade <42661561+andradenathan@users.noreply.github.com>
2021-05-18 00:53:31 -0300

    + Algoritmo de primos k-gêmeos

---
+++ exercicios/gemeosPrimos.py
@@ -0,0 +1,43 @@
+def crivo_erastotenes(n):
+	"""
+	Crivo de erastótenes é um gerador de números primos até um certo limite n
+	:param n: Limitador natural n >= 2
+	:return: Lista de todos os números primos <= n
+	"""
+	lista = [True] * n
+	lista_primos = []
+
+	lista[0] = False # 0 não é primo
+	lista[1] = False # 1 não é primo
+
+	for i in range(2, n):
+		if lista[i]:
+			for j in range(i ** 2, n, i):
+				lista[j] = False
+
+	for k in range(2, n):
+		if lista[k]:
+			lista_primos.append(k)
+
+	return lista_primos
+
+def gemeos_primos(k1, k2, limite):
+	"""
+	k-gêmeos são definidos por números primos que distam k um do outro
+	param k1: int -> int
+	param k2: int -> int
+	param limite: int -> int
+	return: list -> list
+	"""
+	lista_primos = crivo_erastotenes(limite)
+	lista_kgemeos_primos = []
+	for k in range(len(lista_primos)):
+		if (lista_primos[k] + k1) in lista_primos:
+			lista_kgemeos_primos.append([lista_primos[k], lista_primos[k] + k1])
+
+		if (lista_primos[k] + k2) in lista_primos:
+			lista_kgemeos_primos.append([lista_primos[k], lista_primos[k] + k2])
+
+	return lista_kgemeos_primos
+
+print(gemeos_primos(2, 4, 100000))
